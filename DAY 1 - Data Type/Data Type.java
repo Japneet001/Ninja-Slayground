90419e90f7f9b805f645eb6286f12523c7d7963b
Japneet Kaur <jkaur01022004@gmail.com>
2023-08-13 12:57:52 +0530

    Update Data Type.java

--- DAY 1 - Data Type/Data Type.java
+++ DAY 1 - Data Type/Data Type.java
@@ -1,3 +1,12 @@
+/*  Data type refers to the type of value a variable has and the way the computer interprets it.
+    Each data type has a different size. You've studied 5 different data types and the sizes of the data types:
+        Integer: 4 bytes
+        Long: 8 bytes
+        Float: 4 bytes 
+        Double: 8 bytes
+        Character: 1 byte
+    You're given a data type. Print its size in bytes. 
+*/
 
 import java.util.*;
 
